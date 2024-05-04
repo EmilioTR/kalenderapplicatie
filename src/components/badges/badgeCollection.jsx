a8ffa45cde2e4fa275108d184d6dcb1354b842c0
Emilio-TR <Emilio.TenaRomero@hotmail.com>
2024-05-04 13:03:41 +0200

    added a badgecollection

---
+++ src/components/badges/badgeCollection.jsx
@@ -0,0 +1,72 @@
+import Drawer from '@mui/joy/Drawer';
+import ModalClose from '@mui/joy/ModalClose';
+import DialogTitle from '@mui/joy/DialogTitle';
+import DialogContent from '@mui/joy/DialogContent';
+import Sheet from '@mui/joy/Sheet';
+import Divider from '@mui/joy/Divider';
+import Image from 'next/image';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
+
+export default function BadgeCollecion({ openCollection, setOpenCollection, level }) {
+
+    return (
+        <Drawer
+            open={openCollection}
+            onClose={() => setOpenCollection(false)}
+            anchor='left'
+            size='sm'
+
+            slotProps={{
+                content: {
+                    sx: {
+                        bgcolor: 'transparent',
+                        p: { md: 3, sm: 0 },
+                        boxShadow: 'none',
+                        
+                    },
+                },
+            }}
+        >
+
+            <Sheet
+                sx={{
+                    borderRadius: 'md',
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                    height: '100%',
+                    overflow: 'auto',
+                }}
+            >
+
+                <ModalClose />
+                <DialogTitle className="text-violet-800" >Badge verzameling</DialogTitle>
+                <Divider sx={{ mt: 'auto', background: 'darkviolet' }} />
+                <DialogContent className="flex w-full flex-col items-center">
+                    <div className='w-full'>
+                        {level === 0 ?
+                            <div> Je hebt momenteel nog geen badges... </div> :
+                            <div> Jouw badges: </div>
+                        }
+                    </div>
+                    <List>
+
+                        {Array.from({ length: level }, (v, i) => (
+                            <ListItem key={i}>
+                                <Image alt="badge" width={180} height={180} src={`/images/badgelvl${i + 1}.png`} />
+                            </ListItem>
+                        ))}
+
+                    </List>
+
+
+                </DialogContent>
+
+            </Sheet>
+        </Drawer>
+    )
+
+}
--- src/components/badges/progressBar.jsx
+++ src/components/badges/progressBar.jsx
@@ -5,26 +5,28 @@ import { Box } from "@mui/material";
 
 import ConfettiPoppers from '@/components/badges/confettiPoppers';
 import Image from 'next/image';
-import CongratulationDialog from '@/components/badges/congratulationDialog'; 
+import CongratulationDialog from '@/components/badges/congratulationDialog';
+import BadgeCollecion from '@/components/badges/badgeCollection'
 
 export default function ProgressBar({
     doneTodos,
 }) {
 
     useEffect(() => {
-        if (doneTodos.length === 0 )
-            setShowProgressNumber( 0 )
-        else if (doneTodos.length%5 === 0) {
+        if (doneTodos.length === 0)
+            setShowProgressNumber(0)
+        else if (doneTodos.length % 5 === 0) {
             setShowProgressNumber(100)
             setIsConfettiVisible(true);
             setShowCongratulations(true)
-            setLevel(Math.floor(doneTodos.length/5))
+            setLevel(Math.floor(doneTodos.length / 5))
             setTimeout(() => {
-                setShowProgressNumber(doneTodos.length%5 * 20);
+                setShowProgressNumber(doneTodos.length % 5 * 20);
                 setIsConfettiVisible(false)
-              }, 1500);}
-        else{
-            setShowProgressNumber( doneTodos.length%5 * 20)
+            }, 1500);
+        }
+        else {
+            setShowProgressNumber(doneTodos.length % 5 * 20)
         }
     }, [doneTodos])
 
@@ -33,36 +35,41 @@ export default function ProgressBar({
     const [isConfettiVisible, setIsConfettiVisible] = useState(false);
     const [showCongratulations, setShowCongratulations] = useState(false);
     const [level, setLevel] = useState(0);
+    const [openCollection, setOpenCollection] = useState(false);
 
 
     return (
         <>
-        {isConfettiVisible
-            &&
-            <ConfettiPoppers/>
-        }
+            {isConfettiVisible
+                &&
+                <ConfettiPoppers />
+            }
 
-        <div className="flex flex-col justify-center" >
-            
-            {/* <div className="flex flex-row justify-between" >
-                <p>Score: {doneTodos.length}</p> */}
-                {/* <button
-                    className="inline-flex justify-center rounded-md border p-1 border-transparent items-center bg-violet-100 text-sm font-medium text-violet-900 hover:bg-violet-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={() => console.log(allTodos)}
-                    > Progress
-                </button> */}
-            {/* </div> */}
+            <div className="flex flex-col justify-center" >
+                <div className="flex flex-row justify-between mx-5"> 
+                    <div>
+                        Level: {level}
+                    </div>
+                    <button
+                        className="inline-flex justify-center rounded-md border w-fit py-1 px-3 border-transparent items-center bg-violet-100 text-sm font-medium text-violet-900 hover:bg-violet-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        onClick={() => setOpenCollection(true)}
+                    >
+                        Verzameling
+                    </button>
+                </div>
 
-            <div className="flex flex-row gap-1 items-center">
-                <Image alt="current" width={80} height={80}  src={`/images/badgelvl${level}.png`} />
-                <Box className="w-36">
-                    <LinearProgress variant="determinate" value={showProgressNumber} />
-                </Box>
-                <Image alt="next" width={80} height={80} src={`/images/badgelvl${level + 1}.png`} />
+                <div className="flex flex-row gap-1 items-center">
+                    <Image alt="current" width={80} height={80} src={`/images/badgelvl${level}.png`} />
+                    <Box className="w-36">
+                        <LinearProgress variant="determinate" value={showProgressNumber} />
+                    </Box>
+                    <Image alt="next" width={80} height={80} src={`/images/badgelvl${level + 1}.png`} />
 
-            </div>
+                </div>
+
                 <CongratulationDialog {...{ showCongratulations, setShowCongratulations, level }} />
-        </div>
-    </>
+                <BadgeCollecion {...{ openCollection, setOpenCollection, level }}></BadgeCollecion>
+            </div>
+        </>
     )
 }
